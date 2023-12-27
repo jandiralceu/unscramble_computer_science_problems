04298aecd5b571bc3f4ff0b8910638f38c0e770a
Jandir A. Cutabiala <me@jandir.co>
2023-12-27 20:13:44 -0300

    feat: complete the task 0 printing the first record from texts.csv and the las from calls.csv

--- Task0.py
+++ Task0.py
@@ -3,6 +3,7 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -12,11 +13,20 @@ with open('calls.csv', 'r') as f:
     calls = list(reader)
 
 
-"""
-TASK 0:
-What is the first record of texts and what is the last record of calls?
-Print messages:
-"First record of texts, <incoming number> texts <answering number> at time <time>"
-"Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
-"""
+def get_date_and_time(date):
+    """This function takes a date as input and extracts the date and time components."""
+    result = date.split()
+    return result[0], result[1]
 
+
+first_texts_record = texts[0]
+first_texts_record_date_time = get_date_and_time(first_texts_record[2])
+
+last_calls_record = calls[-1]
+last_calls_record_date_time = get_date_and_time(last_calls_record[2])
+
+print(f"First record of texts, {first_texts_record[0]} texts {first_texts_record[1]} "
+      f"at time {first_texts_record_date_time[1]}")
+print(f"Last record of calls, {last_calls_record[0]} calls {last_calls_record[1]} "
+      f"at time {last_calls_record_date_time[1]},"
+      f" lasting {last_calls_record[3]} seconds")
