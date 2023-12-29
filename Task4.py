0d6b83854b2782bd7cb7d950b6dfecb322f97d3b
Jandir A. Cutabiala <me@jandir.co>
2023-12-29 17:44:15 -0300

    feat: complete task 4 print all possible telemarketers numbers

--- Task4.py
+++ Task4.py
@@ -25,3 +25,39 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+all_contacts = set()
+
+for entry in calls:
+    all_contacts.add(entry[0])
+    all_contacts.add(entry[1])
+    
+for entry in texts:
+    all_contacts.add(entry[0])
+    all_contacts.add(entry[1])
+
+
+def has_sent_or_received_message(number: str) -> bool:
+    '''Checks whether a number has sent or received a message.'''
+    for entry in texts:
+        if entry[0] == number or entry[1] == number:
+            return True
+        
+    return False
+
+def has_income_call(number: str) -> bool:
+    '''Checks whether a number has received an income call.'''
+    for entry in calls:
+        if entry[1] == number:
+            return True
+        
+    return False
+
+for number in all_contacts.copy():
+    if has_sent_or_received_message(number) or has_income_call(number):
+        all_contacts.remove(number)
+
+
+print("These numbers could be telemarketers: ")
+for number in sorted(all_contacts):
+    print(number)
